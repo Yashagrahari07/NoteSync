5cc490b627fa49229252669701231f0e7afc4657
Yash Agrahari <yashagrahari456@gmail.com>
2025-05-02 23:09:00 +0530

    user login endpoint done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,7 +2,7 @@ const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
 
-module.exports.registerUser = async (req, res, next) => {
+const registerUser = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -37,4 +37,47 @@ module.exports.registerUser = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
+};
+
+const loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const token = user.generateAuthToken();
+
+        const { password: _, ...userData } = user.toObject();
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 24 * 60 * 60 * 1000
+        });
+
+        res.status(200).json({ token, user: userData });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = {
+    registerUser,
+    loginUser
 };

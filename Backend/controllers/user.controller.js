1de1769c3dcd31c9f2b0901dab3bd0b9e1c26066
Yash Agrahari <yashagrahari456@gmail.com>
2025-05-02 23:27:00 +0530

    user authentication and logout endpoint done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 const registerUser = async (req, res, next) => {
     const errors = validationResult(req);
@@ -77,7 +78,22 @@ const loginUser = async (req, res, next) => {
     }
 };
 
+const getUserProfile = async (req, res, next) => {
+    res.status(200).json(req.user);
+}
+
+const logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blacklistTokenModel.create({ token });
+
+    res.status(200).json({ message: 'Logged out successfully' });
+};
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getUserProfile,
+    logoutUser
 };

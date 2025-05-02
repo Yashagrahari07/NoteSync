27604e549999f3e334761f6b94f1eb44c926f651
Yash Agrahari <yashagrahari456@gmail.com>
2025-05-02 23:47:00 +0530

    added note model

---
+++ Backend/models/note.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String],
+    default: []
+  },
+  isPinned: {
+    type: Boolean,
+    default: false
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true
+  },
+  collaborators: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user"
+    }
+  ],
+  quote: {
+    text: { type: String }
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  },
+  updatedOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+noteSchema.pre("save", function (next) {
+  this.updatedOn = Date.now();
+  next();
+});
+
+module.exports = mongoose.model("note", noteSchema);

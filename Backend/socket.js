4bbace3a172faa81e493a19a5589eedf6a098765
Yash Agrahari <yashagrahari456@gmail.com>
2025-05-03 12:16:00 +0530

    added jwt auth middleware for websocket

--- Backend/socket.js
+++ Backend/socket.js
@@ -1,4 +1,31 @@
+const jwt = require('jsonwebtoken');
+const { userModel } = require('./models/user.model');
 
 module.exports.setupSocket = (server) => {
+  const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  });
 
-}
+  io.use(async (socket, next) => {
+    try {
+      const token = socket.handshake.auth.token;
+      if (!token) {
+        return next(new Error("Unauthorized"));
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded._id);
+      if (!user) {
+        return next(new Error("Unauthorized"));
+      }
+
+      socket.user = user;
+      next();
+    } catch (err) {
+      next(new Error("Unauthorized"));
+    }
+  });
+};

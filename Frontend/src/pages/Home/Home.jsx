552884405990ee9676e3c95f023875780496237a
Yash Agrahari <yashagrahari456@gmail.com>
2025-05-03 17:33:00 +0530

    Created Note Card UI

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import NoteCard from '../../components/Cards/NoteCard';
+import { MdAdd } from 'react-icons/md';
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-3 gap-4 mt-8'>
+          <NoteCard 
+            title='Meeting on 7th May' 
+            date='3rd May 2025' 
+            content='Meeting on 7th May' 
+            tags='#Meeting'
+            onEdit={() => {}}
+            onDelete={() => {}}
+            OnPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={() => {}}>
+        <MdAdd className='text-[32px] text-white' />
+      </button>
     </>
   )
 }
